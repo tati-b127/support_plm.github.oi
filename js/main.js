62513998ff5d24f967f9b185d21f4f8e4e262105
tati-b127 <101515412+tati-b127@users.noreply.github.com>
2022-07-05 14:30:18 +0300

    v_05_07_22

---
+++ js/main.js
@@ -0,0 +1,17 @@
+document.addEventListener('DOMContentLoaded', function() {
+    document.querySelectorAll('.menu__btn').forEach(function(tabLink) {
+        tabLink.addEventListener('click', function(event) {
+            const path = event.currentTarget.dataset.path;
+            document.querySelectorAll('.menu__btn').forEach(function(link) {
+                link.classList.remove('menu__btn_active')});
+                event.currentTarget.classList.add('menu__btn_active');
+            document.querySelectorAll('.category__group').forEach(function(tabContent) {
+                tabContent.classList.remove('category__group_visible')});
+            document.querySelector(`[data-target="${path}"]`).classList.add('category__group_visible');
+			// document.querySelector(`[data-target="${path}"]`).scrollIntoView({
+			// 	behavior: 'smooth',
+			// 	block: 'start'
+    });
+		});
+
+        });
